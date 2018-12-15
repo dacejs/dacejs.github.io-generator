4d468d96b36da07b1367a0a96cdde3110ce1e2ae
zhongzhi107 <zhong.zhi@163.com>
2018-12-16 06:09:10 +0800

    增加 create-dace-app 动画

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -100,10 +100,10 @@ class Index extends React.Component {
 要创建名为 \`my-app\` 的项目，请运行以下命令：
 
 \`\`\`sh
-npx create-react-app my-app
+npx create-dace-app my-app
 \`\`\`
 `,
-            image: 'https://camo.githubusercontent.com/29765c4a32f03bd01d44edef1cd674225e3c906b/68747470733a2f2f63646e2e7261776769742e636f6d2f66616365626f6f6b2f6372656174652d72656163742d6170702f323762343261632f73637265656e636173742e737667',
+            image: `${baseUrl}img/create-dace-app-480.gif`,
             imageAlign: 'right',
             title: '快速入门',
           },
